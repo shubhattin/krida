3d85e4cdb55502538b39b45293ea8fc3372cabe7
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-05-20 11:42:49 +0530

    ky ram

--- src/lib/get_auth_from_cookie.ts
+++ src/lib/get_auth_from_cookie.ts
@@ -1,18 +1,17 @@
-import ky from 'ky';
 import type { authClient } from '@/lib/auth-client';
 
 const get_seesion_from_cookie = async (cookie: string) => {
   try {
-    const session = await ky
-      .get<typeof authClient.$Infer.Session>(
-        `${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/api/auth/get-session`,
-        {
-          headers: {
-            Cookie: cookie
-          }
-        }
-      )
-      .json();
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/api/auth/get-session`, {
+      method: 'GET',
+      headers: {
+        Cookie: cookie
+      }
+    });
+    if (!res.ok) {
+      throw new Error(`Failed to fetch session: ${res.statusText}`);
+    }
+    const session = (await res.json()) as typeof authClient.$Infer.Session;
     return session;
   } catch (e) {
     return null;
