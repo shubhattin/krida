363f7d0887c50e410d2ab00a2361130643a25586
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-05-21 17:19:45 +0530

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,8 @@ import { Metadata } from 'next';
 import WordGame from '~/components/pages/main/WordGame';
 import Others from './OtherLinks';
 
-// Force this route to be dynamic so cache is not static and random values update on each request
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
-const get_rand_num = (a: number, b: number) => {
-  return Math.trunc(Math.random() * (b - a + 1)) + a;
-};
-
 export default async function Home() {
+  const currentTime = new Date();
   const list = await db.query.word_puzzles.findMany({
     columns: {
       id: true
@@ -20,7 +13,7 @@ export default async function Home() {
     orderBy: ({ created_at }, { desc }) => desc(created_at)
   });
 
-  const randomIndex = get_rand_num(0, list.length - 1);
+  const randomIndex = currentTime.getSeconds() % list.length;
   const word_puzzle = (await db.query.word_puzzles.findFirst({
     where: ({ id }, { eq }) => eq(id, list[randomIndex].id)
   }))!;
@@ -35,10 +28,7 @@ export default async function Home() {
           <div className="flex flex-col items-center">
             <span className="text-xl font-bold">{word_puzzle.title}</span>
           </div>
-          <div>
-            {list.length}: {randomIndex} ({get_rand_num(0, 10)})
-          </div>
-          <div>Curent Server Time : {new Date().toLocaleString()}</div>
+          <div>Curent Server Time : {currentTime.toLocaleString()}</div>
           <WordGame
             grid_data={word_puzzle.grid_data}
             dims={[6, 6]}
