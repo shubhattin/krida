f61bc11cc68ba8e9dc8f092b4b7a9d44937bad4e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-05-23 23:38:28 +0530

    glitch

--- src/app/view/[uuid_id]/page.tsx
+++ src/app/view/[uuid_id]/page.tsx
@@ -51,28 +51,30 @@ const MainEdit = async ({ params }: Props) => {
   );
 
   return (
-    <>
-      <div className="my-2 mb-3.5 px-2">
-        <Link href="/" className="flex items-center gap-1 text-lg font-semibold">
-          <IoMdArrowRoundBack className="inline-block text-xl" />
-          मुख्यपृष्ठम्
-        </Link>
+    <main className="flex flex-1 items-center justify-center p-4">
+      <div className="mt-6 sm:mt-10">
+        <div className="my-2 mb-3.5 px-2">
+          <Link href="/" className="flex items-center gap-1 text-lg font-semibold">
+            <IoMdArrowRoundBack className="inline-block text-xl" />
+            मुख्यपृष्ठम्
+          </Link>
+        </div>
+        {word_puzzle ? (
+          <>
+            <WordGame
+              script_init={script}
+              title={word_puzzle.title}
+              word_list={word_puzzle.word_list}
+              dims={word_puzzle.grid_dimensions}
+              grid_data={word_puzzle.grid_data}
+              initial_script_data={{ word_msgs: word_game_msgs, title, grid_data }}
+            />
+          </>
+        ) : (
+          <div>अनुचित ID</div>
+        )}
       </div>
-      {word_puzzle ? (
-        <>
-          <WordGame
-            script_init={script}
-            title={word_puzzle.title}
-            word_list={word_puzzle.word_list}
-            dims={word_puzzle.grid_dimensions}
-            grid_data={word_puzzle.grid_data}
-            initial_script_data={{ word_msgs: word_game_msgs, title, grid_data }}
-          />
-        </>
-      ) : (
-        <span>अनुचित ID</span>
-      )}
-    </>
+    </main>
   );
 };
 
