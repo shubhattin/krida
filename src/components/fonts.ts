cef6731a6512c9e19afc16febc3f4c11a10bcb46
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-05-23 18:20:03 +0530

    multiple font support

--- src/components/fonts.ts
+++ src/components/fonts.ts
@@ -1,4 +1,11 @@
-import { Noto_Sans_Devanagari, Roboto, Noto_Sans_Telugu } from 'next/font/google';
+import {
+  Noto_Sans_Devanagari,
+  Roboto,
+  Noto_Sans_Telugu,
+  Noto_Sans_Kannada,
+  Noto_Sans_Gujarati,
+  Noto_Sans_Bengali
+} from 'next/font/google';
 
 export const robotoSans = Roboto({
   variable: '--font-roboto-sans',
@@ -13,7 +20,25 @@ export const notoSansDevanagari = Noto_Sans_Devanagari({
 });
 
 export const notoSansTelugu = Noto_Sans_Telugu({
-  variable: '--font-noto-sans-telugu',
+  variable: '--font-noto-serif-telugu',
   subsets: ['telugu'],
+  weight: ['400', '500', '700']
+});
+
+export const notoSansKannada = Noto_Sans_Kannada({
+  variable: '--font-noto-serif-kannada',
+  subsets: ['kannada'],
+  weight: ['400', '500', '700']
+});
+
+export const notoSansGujarati = Noto_Sans_Gujarati({
+  variable: '--font-noto-serif-gujarati',
+  subsets: ['gujarati'],
+  weight: ['400', '500', '700']
+});
+
+export const notoSansBengali = Noto_Sans_Bengali({
+  variable: '--font-noto-serif-bengali',
+  subsets: ['bengali'],
   weight: ['400', '500', '700']
 });
--- src/state/main.state.ts
+++ src/state/main.state.ts
@@ -1,11 +1,17 @@
-import { atom } from 'jotai';
-import { notoSansDevanagari, notoSansTelugu } from '~/components/fonts';
+import { atomWithStorage } from 'jotai/utils';
+import {
+  notoSansDevanagari,
+  notoSansKannada,
+  notoSansTelugu,
+  notoSansBengali,
+  notoSansGujarati
+} from '~/components/fonts';
 
-export const SCRIPT_LIST = ['Devanagari', 'Telugu'] as const;
+export const SCRIPT_LIST = ['Devanagari', 'Telugu', 'Kannada', 'Gujarati', 'Bengali'] as const;
 export type ScriptType = (typeof SCRIPT_LIST)[number];
 
 export const DEFAULT_DATA_SCRIPT: ScriptType = 'Devanagari';
-export const script_atom = atom<ScriptType>('Devanagari');
+export const script_atom = atomWithStorage<ScriptType>('default_data_script', 'Devanagari');
 
 export const FONT_INFO: Record<ScriptType, { clasName: string; fontSize: number }> = {
   Devanagari: {
@@ -14,6 +20,18 @@ export const FONT_INFO: Record<ScriptType, { clasName: string; fontSize: number 
   },
   Telugu: {
     clasName: notoSansTelugu.className,
-    fontSize: 1.02
+    fontSize: 1.15
+  },
+  Kannada: {
+    clasName: notoSansKannada.className,
+    fontSize: 1.01
+  },
+  Bengali: {
+    clasName: notoSansBengali.className,
+    fontSize: 1.25
+  },
+  Gujarati: {
+    clasName: notoSansGujarati.className,
+    fontSize: 1.25
   }
 };
