73fe5fec55039aed330526186b853d313c1f0263
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-06-11 10:07:44 +0530

    init

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,14 @@
-import { pgTable, serial, jsonb, text, timestamp, index, uuid, integer } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  jsonb,
+  text,
+  timestamp,
+  index,
+  uuid,
+  integer,
+  boolean
+} from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
 
 export const word_puzzles = pgTable(
@@ -11,7 +21,8 @@ export const word_puzzles = pgTable(
     updated_at: timestamp({ withTimezone: true }), // NULL for not updated
     word_list: jsonb().notNull().$type<string[]>(),
     grid_data: jsonb().notNull().$type<string[][]>(),
-    grid_dimensions: jsonb().notNull().$type<[number, number]>()
+    grid_dimensions: jsonb().notNull().$type<[number, number]>(),
+    archived: boolean().notNull().default(false)
   },
   (table) => [index().on(table.created_at)]
 );
@@ -28,15 +39,32 @@ export const puzzle_gameplay_stats = pgTable('puzzle_gameplay_stats', {
   total_attempts: integer().notNull()
 });
 
+export const puzzle_game_schedules = pgTable('puzzle_game_schedules', {
+  id: serial().primaryKey(),
+  puzzle_id: integer()
+    .notNull()
+    .references(() => word_puzzles.id, { onDelete: 'cascade' }),
+  start_time: timestamp({ withTimezone: true }).notNull(),
+  end_time: timestamp({ withTimezone: true }).notNull()
+});
+
 // relations
 
 export const word_puzzlesRelations = relations(word_puzzles, ({ many }) => ({
-  stats: many(puzzle_gameplay_stats)
+  stats: many(puzzle_gameplay_stats),
+  schedules: many(puzzle_game_schedules)
 }));
 
 export const puzzle_gameplay_statsRelations = relations(puzzle_gameplay_stats, ({ one }) => ({
   puzzle: one(word_puzzles, {
     fields: [puzzle_gameplay_stats.puzzle_id],
+    references: [word_puzzles.id]
+  })
+}));
+
+export const puzzle_game_schedulesRelations = relations(puzzle_game_schedules, ({ one }) => ({
+  puzzle: one(word_puzzles, {
+    fields: [puzzle_game_schedules.puzzle_id],
     references: [word_puzzles.id]
   })
 }));
