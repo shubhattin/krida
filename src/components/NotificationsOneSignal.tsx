bb7e7709466ab1150376be1ba9bb7f002024e832
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-07-19 19:57:45 +0530

    update

--- src/components/NotificationsOneSignal.tsx
+++ src/components/NotificationsOneSignal.tsx
@@ -7,6 +7,7 @@ export default function Page() {
   useEffect(() => {
     // Ensure this code runs only on the client side
     if (
+      process.env.NODE_ENV === 'production' &&
       typeof window !== 'undefined' &&
       process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID &&
       process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_WEB_ID
@@ -16,9 +17,35 @@ export default function Page() {
         safari_web_id: process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_WEB_ID,
         // @ts-ignore
         notifyButton: {
-          enable: true
+          enable: false
         },
-        serviceWorkerPath: '/onesignal/OneSignalSDKWorker.js'
+        serviceWorkerParam: {
+          scope: '/onesignal/'
+        },
+        serviceWorkerPath: 'onesignal/OneSignalSDKWorker.js',
+        promptOptions: {
+          // @ts-ignore
+          customlink: {
+            enabled: true,
+            style: 'button',
+            size: 'medium',
+            color: {
+              button: '#E12D30', // button background color
+              text: '#FFFFFF' // button text color
+            },
+            text: {
+              subscribe: 'Subscribe to Padavali notifications',
+              explanation: 'Subscribe to get notified for new Padavali puzzles'
+            },
+            unsubscribeEnabled: false
+          }
+        },
+        welcomeNotification: {
+          disable: false,
+          message: 'You are Successfully Subscribed to Padavali Updates',
+          title: 'Welcome!',
+          url: 'https://krida.thesanskritchannel.org/padavali' // Optional
+        }
       });
     }
   }, []);
