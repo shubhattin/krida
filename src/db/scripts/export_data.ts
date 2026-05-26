35aa5ddce784cbcc4422be3ec2b0dedec1968256
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-05-26 21:03:23 +0530

    tx

--- src/db/scripts/export_data.ts
+++ src/db/scripts/export_data.ts
@@ -1,4 +1,4 @@
-import { dbClient_ext as db, queryClient } from './client';
+import { dbClient_ext, queryClient } from './client';
 import { readFile } from 'fs/promises';
 import { dbMode, take_input } from '~/tools/kry.server';
 import {
@@ -45,100 +45,102 @@ const main = async () => {
     })
     .parse(JSON.parse((await readFile(`./out/${in_file_name}`)).toString()));
 
-  // deleting all the tables initially
-  try {
-    await db.delete(word_puzzles);
-    await db.delete(word_puzzle_attachments);
-    await db.delete(puzzle_gameplay_stats);
-    await db.delete(puzzle_game_schedules);
-    await db.delete(puzzle_gameplay_sessions);
-    console.log(chalk.green('✓ Deleted All Tables Successfully'));
-  } catch (e) {
-    console.log(chalk.red('✗ Error while deleting tables:'), chalk.yellow(e));
-  }
-
-  // inserting word_puzzles
-  try {
-    await db.insert(word_puzzles).values(data.word_puzzles);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`word_puzzles`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting word_puzzles:'), chalk.yellow(e));
-  }
-
-  // inserting word_puzzle_attachments
-  try {
-    await db.insert(word_puzzle_attachments).values(data.word_puzzle_attachments);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`word_puzzle_attachments`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting word_puzzle_attachments:'), chalk.yellow(e));
-  }
-
-  // inserting puzzle_game_schedules
-  try {
-    await db.insert(puzzle_game_schedules).values(data.puzzle_game_schedules);
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`puzzle_game_schedules`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting puzzle_game_schedules:'), chalk.yellow(e));
-  }
-
-  // inserting puzzle_gameplay_sessions
-  try {
-    const chunks = chunkArray(data.puzzle_gameplay_sessions, 5000);
-    for (const chunk of chunks) {
-      await db.insert(puzzle_gameplay_sessions).values(chunk);
+  const tx = await dbClient_ext.transaction(async (tx) => {
+    // deleting all the tables initially
+    try {
+      await tx.delete(word_puzzles);
+      await tx.delete(word_puzzle_attachments);
+      await tx.delete(puzzle_gameplay_stats);
+      await tx.delete(puzzle_game_schedules);
+      await tx.delete(puzzle_gameplay_sessions);
+      console.log(chalk.green('✓ Deleted All Tables Successfully'));
+    } catch (e) {
+      console.log(chalk.red('✗ Error while deleting tables:'), chalk.yellow(e));
     }
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`puzzle_gameplay_sessions`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting puzzle_gameplay_sessions:'), chalk.yellow(e));
-  }
 
-  // inserting puzzle_gameplay_stats
-  try {
-    const chunks = chunkArray(data.puzzle_gameplay_stats, 5000);
-    for (const chunk of chunks) {
-      await db.insert(puzzle_gameplay_stats).values(chunk);
+    // inserting word_puzzles
+    try {
+      await tx.insert(word_puzzles).values(data.word_puzzles);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`word_puzzles`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting word_puzzles:'), chalk.yellow(e));
     }
-    console.log(
-      chalk.green('✓ Successfully added values into table'),
-      chalk.blue('`puzzle_gameplay_stats`')
-    );
-  } catch (e) {
-    console.log(chalk.red('✗ Error while inserting puzzle_gameplay_stats:'), chalk.yellow(e));
-  }
 
-  // resetting SERIAL
-  try {
-    await db.execute(
-      sql`SELECT setval('"word_puzzles_id_seq"', (select MAX(id) from "word_puzzles"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"word_puzzle_attachments_id_seq"', (select MAX(id) from "word_puzzle_attachments"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"puzzle_gameplay_stats_id_seq"', (select MAX(id) from "puzzle_gameplay_stats"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"puzzle_game_schedules_id_seq"', (select MAX(id) from "puzzle_game_schedules"))`
-    );
-    await db.execute(
-      sql`SELECT setval('"puzzle_gameplay_sessions_id_seq"', (select MAX(id) from "puzzle_gameplay_sessions"))`
-    );
-    console.log(chalk.green('✓ Successfully resetted ALL SERIAL'));
-  } catch (e) {
-    console.log(chalk.red('✗ Error while resetting SERIAL:'), chalk.yellow(e));
-  }
+    // inserting word_puzzle_attachments
+    try {
+      await tx.insert(word_puzzle_attachments).values(data.word_puzzle_attachments);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`word_puzzle_attachments`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting word_puzzle_attachments:'), chalk.yellow(e));
+    }
+
+    // inserting puzzle_game_schedules
+    try {
+      await tx.insert(puzzle_game_schedules).values(data.puzzle_game_schedules);
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`puzzle_game_schedules`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting puzzle_game_schedules:'), chalk.yellow(e));
+    }
+
+    // inserting puzzle_gameplay_sessions
+    try {
+      const chunks = chunkArray(data.puzzle_gameplay_sessions, 5000);
+      for (const chunk of chunks) {
+        await tx.insert(puzzle_gameplay_sessions).values(chunk);
+      }
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`puzzle_gameplay_sessions`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting puzzle_gameplay_sessions:'), chalk.yellow(e));
+    }
+
+    // inserting puzzle_gameplay_stats
+    try {
+      const chunks = chunkArray(data.puzzle_gameplay_stats, 5000);
+      for (const chunk of chunks) {
+        await tx.insert(puzzle_gameplay_stats).values(chunk);
+      }
+      console.log(
+        chalk.green('✓ Successfully added values into table'),
+        chalk.blue('`puzzle_gameplay_stats`')
+      );
+    } catch (e) {
+      console.log(chalk.red('✗ Error while inserting puzzle_gameplay_stats:'), chalk.yellow(e));
+    }
+
+    // resetting SERIAL
+    try {
+      await tx.execute(
+        sql`SELECT setval('"word_puzzles_id_seq"', (select MAX(id) from "word_puzzles"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"word_puzzle_attachments_id_seq"', (select MAX(id) from "word_puzzle_attachments"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"puzzle_gameplay_stats_id_seq"', (select MAX(id) from "puzzle_gameplay_stats"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"puzzle_game_schedules_id_seq"', (select MAX(id) from "puzzle_game_schedules"))`
+      );
+      await tx.execute(
+        sql`SELECT setval('"puzzle_gameplay_sessions_id_seq"', (select MAX(id) from "puzzle_gameplay_sessions"))`
+      );
+      console.log(chalk.green('✓ Successfully resetted ALL SERIAL'));
+    } catch (e) {
+      console.log(chalk.red('✗ Error while resetting SERIAL:'), chalk.yellow(e));
+    }
+  });
 };
 main().then(() => {
   queryClient.end();
