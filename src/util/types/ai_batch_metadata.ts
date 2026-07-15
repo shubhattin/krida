974d23e93595ad23fdf6b1766df0fb11c5d3d646
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-07-15 20:42:15 +0530

    3 mins polling iterval for batch reposnes

--- src/util/types/ai_batch_metadata.ts
+++ src/util/types/ai_batch_metadata.ts
@@ -8,7 +8,7 @@ import ms from 'ms';
 */
 
 /** Polling Interval for Batch API via QStash */
-export const BATCH_POLLING_INTERVAL_S = ms('5mins') / 1000;
+export const BATCH_POLLING_INTERVAL_S = ms('3mins') / 1000;
 
 /** OpenAI batch completion window is 24h; stop QStash retries after that. */
 export const MAX_BATCH_POLL_ATTEMPTS = Math.ceil((24 * 60 * 60) / BATCH_POLLING_INTERVAL_S);
